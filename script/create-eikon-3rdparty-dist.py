ae6a00961bb83465ade89fd8796e7914e748466d
Michael Vasseur <michael.vasseur@thomsonreuters.com>
2016-09-20 14:16:54 +0200

    Updated Eikon 3rdParty distribution script to use IOJS binaries from the Atom Shell server

--- script/create-eikon-3rdparty-dist.py
+++ script/create-eikon-3rdparty-dist.py
@@ -7,6 +7,8 @@ import shutil
 import subprocess
 import sys
 import stat
+import urllib
+import tarfile
 
 from lib.config import LIBCHROMIUMCONTENT_COMMIT, BASE_URL, PLATFORM, \
                        get_target_arch, get_chromedriver_version, \
@@ -88,7 +90,12 @@ def main():
   copy_libraries()
   copy_headers()
   create_zip()
+    
+  rm_rf(EIKON_3RDPARTY_DIST_DIR)
+  
+  # upload_to_azure()
 
+  
 def force_build():
   build = os.path.join(SOURCE_ROOT, 'script', 'build.py')
   execute([sys.executable, build, '-c', 'Release'])
@@ -109,26 +116,34 @@ def copy_binaries():
 def copy_libraries():
   
   os.makedirs(DIST_LIB_DIR)
-  
-  shutil.copy2(os.path.join(OUT_DIR, 'node.dll.lib'), os.path.join(DIST_LIB_DIR, 'electron.lib'))
+  urllib.urlretrieve('https://atom.io/download/atom-shell/' + ELECTRON_VERSION + '/win-x64/iojs.lib', os.path.join(DIST_LIB_DIR, 'electron.lib'))
 
   
 def copy_headers():
 
   os.makedirs(DIST_INC_DIR)
   
+  # Download and extract archive from Atom Shell server
+  iojs_targz_file = os.path.join(EIKON_3RDPARTY_DIST_DIR, 'iojs.tar.gz')
+  urllib.urlretrieve('https://atom.io/download/atom-shell/' + ELECTRON_VERSION + '/iojs-' + ELECTRON_VERSION + '.tar.gz', iojs_targz_file)
+  tfile = tarfile.open(iojs_targz_file, 'r:gz')
+  tfile.extractall(EIKON_3RDPARTY_DIST_DIR)
+  tfile.close()
+  os.remove(iojs_targz_file)
+  iojs_targz_dir = os.path.join(EIKON_3RDPARTY_DIST_DIR, 'iojs-' + ELECTRON_VERSION)
+  
   # Helper header
   shutil.copy2(os.path.join(SOURCE_ROOT, 'eikon-3rdparty-dist', 'includes', 'electron.h'), os.path.join(DIST_INC_DIR))
   
   # Node headers
-  SRC_NODE_DIR = os.path.join(SOURCE_ROOT, 'vendor', 'node', 'src')
+  SRC_NODE_DIR = os.path.join(iojs_targz_dir, 'src')
   DIST_NODE_DIR = os.path.join(DIST_INC_DIR, 'node')
   os.makedirs(DIST_NODE_DIR)
   for inc_file in glob.glob(os.path.join(SRC_NODE_DIR, '*.h')):
     shutil.copy2(os.path.join(SRC_NODE_DIR, inc_file), DIST_NODE_DIR)
 	
   # V8 headers
-  SRC_V8_DIR = os.path.join(SOURCE_ROOT, 'vendor', 'node', 'deps', 'v8', 'include')
+  SRC_V8_DIR = os.path.join(iojs_targz_dir, 'deps', 'v8', 'include')
   DIST_V8_DIR = os.path.join(DIST_INC_DIR, 'v8')
   os.makedirs(DIST_V8_DIR)
   for inc_file in glob.glob(os.path.join(SRC_V8_DIR, '*.h')):
@@ -137,21 +152,21 @@ def copy_headers():
   shutil.copy2(os.path.join(SRC_V8_DIR, 'libplatform', 'libplatform.h'), os.path.join(DIST_V8_DIR, 'libplatform'))
 
   # UV headers
-  SRC_UV_DIR = os.path.join(SOURCE_ROOT, 'vendor', 'node', 'deps', 'uv', 'include')
+  SRC_UV_DIR = os.path.join(iojs_targz_dir, 'deps', 'uv', 'include')
   DIST_UV_DIR = os.path.join(DIST_INC_DIR, 'uv')
   os.makedirs(DIST_UV_DIR)
   for inc_file in glob.glob(os.path.join(SRC_UV_DIR, '*.h')):
     shutil.copy2(os.path.join(SRC_UV_DIR, inc_file), DIST_UV_DIR)
 	
   # ZLib headers
-  SRC_ZLIB_DIR = os.path.join(SOURCE_ROOT, 'vendor', 'node', 'deps', 'zlib')
+  SRC_ZLIB_DIR = os.path.join(iojs_targz_dir, 'deps', 'zlib')
   DIST_ZLIB_DIR = os.path.join(DIST_INC_DIR, 'zlib')
   os.makedirs(DIST_ZLIB_DIR)
   for inc_file in glob.glob(os.path.join(SRC_ZLIB_DIR, '*.h')):
     shutil.copy2(os.path.join(SRC_ZLIB_DIR, inc_file), DIST_ZLIB_DIR)
 	
   # HTTP Parser
-  SRC_HTTP_DIR = os.path.join(SOURCE_ROOT, 'vendor', 'node', 'deps', 'http_parser')
+  SRC_HTTP_DIR = os.path.join(iojs_targz_dir, 'deps', 'http_parser')
   DIST_HTTP_DIR = os.path.join(DIST_INC_DIR, 'http_parser')
   os.makedirs(DIST_HTTP_DIR)
   for inc_file in glob.glob(os.path.join(SRC_HTTP_DIR, '*.h')):
@@ -164,15 +179,23 @@ def copy_headers():
   for inc_file in glob.glob(os.path.join(SRC_NAN_DIR, '*.h')):
     shutil.copy2(os.path.join(SRC_NAN_DIR, inc_file), DIST_NAN_DIR)
 
+  # Clean-up
+  rm_rf(iojs_targz_dir)
 
+  
 def create_version():
   with open(os.path.join(EIKON_3RDPARTY_DIST_DIR, 'version.txt'), 'w') as version_file:
     version_file.write(ELECTRON_VERSION)
+
 	
 def create_zip():
 
-  shutil.make_archive(os.path.join(SOURCE_ROOT, 'eikon-3rdparty-dist', 'out', 'electron', 'electron' + ELECTRON_VERSION), 'zip', EIKON_3RDPARTY_DIST_DIR)
-  rm_rf(EIKON_3RDPARTY_DIST_DIR)
+  shutil.make_archive(os.path.join(SOURCE_ROOT, 'eikon-3rdparty-dist', 'out', 'electron', ELECTRON_VERSION), 'zip', EIKON_3RDPARTY_DIST_DIR)
 
+  
+def upload_to_azure():
+  
+  subprocess.call(os.path.join(SOURCE_ROOT, 'eikon-3rdparty-dist', 'upload3rdparties.exe') + ' -i ' + os.path.join(SOURCE_ROOT, 'eikon-3rdparty-dist', 'out')  + ' -g')
+  
 if __name__ == '__main__':
   sys.exit(main())
